acc0a878e4324832ec71e103122212c20f30e299
Linesetap <129102195+linesetap@users.noreply.github.com>
2024-01-09 18:25:05 +0300

    Update website.py

--- website.py
+++ website.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from threading import *
+import aiohttp
+import asyncio
 
 app = Flask('')
 
@@ -16,3 +18,15 @@ def run():
 def website():
     server = Thread(target=run)
     server.start()
+
+
+async def send_request():
+    url = "https://53c9xj-8080.csb.app/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            # لا يوجد حاجة لقراءة الرد أو القيام بأي شيء آخر هنا
+            pass
